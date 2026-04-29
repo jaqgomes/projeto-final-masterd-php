842f3a6569c43308df408eb9995e6414d34b4e7d
Jaqueline Lima <jgomesribeirolira@gmail.com>
2026-04-29 15:27:09 +0100

    criacao do projects Service do projeto final

--- projects/projectsService.php
+++ projects/projectsService.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'config/Database.php';
+include '../config/Database.php';
 
 class ProjectsService
 {
@@ -10,9 +10,49 @@ class ProjectsService
     {
         $this->database = (new Database())->conexao;
     }
-    public function createProjects($nome_projeto, $descricao, $tecnologia, $tempo_conslusao, $fotografia)
+
+    //este metodo insere um novo registro na tabela projetos usando prepared;criando uma prepared que é uma query segura; (?)sao placeholders
+    //que serão substituidos pelos valores reais(ou seja, todos os campos seraoo tratados como texto);
+    //executa uma query no banco de dados ($stmt);
+    //utilizado quando na criacao de projetos, depois de validar os dados, depois de fazer upload da imagem
+
+    public function createProjects($nome_projeto, $descricao, $tecnologia, $tempo_conclusao, $fotografia)
     {
-       $stmt = $this->database->prepare("INSERT INTO consultas (nome_projeto, descricao, tecnologia, tempo_conslusao, fotografia) VALUES(?,?,?,?,?)""
+        $stmt = $this->database->prepare("INSERT INTO projetos (nome_projeto, descricao, tecnologia, tempo_conslusao, fotografia) VALUES(?,?,?,?,?)");
+        $stmt->bind_param("sssss", $nome_projeto, $descricao, $tecnologia, $tempo_conclusao, $fotografia);
+        return $stmt->execute();
+
+    }
+    //este metodo faz executa uma query no SQL(busca todos registros na tabela projetos),coverte tudo em array;
+    public function getAllProjects()
+    {
+        $result = $this->database->query("SELECT * FROM projetos");
+        return $result->fetch_all(MYSQLI_ASSOC);
+        echo "estou aqui";
+    }
+    //o metodo getProjectsId buscar um projeto pelo ID usando prepared statements
+    //onde ? sera substituido pelo ID
+    public function getProjectsId($id)
+    {
+        $stmt = $this->database->prepare("SELECT * FROM projetos WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_assoc();
+    }
+    //esse metodo atualiza o banco de dados , substituindo pelo que enviar. Apenas com valor do ID que passar 
+    public function updateProjects($id, $nome_projeto, $descricao, $tecnologia, $tempo_conclusao, $fotografia)
+    {
+        $stmt = $this->database->prepare("UPDATE projetos SET nome_projeto = ?, descricao = ?, tecnologia = ?, tempo_conclusao = ?, fotografia = ? WHERE id = ?");
+        $stmt->bind_param("sssssi", $nome_projeto, $descricao, $tecnologia, $tempo_conclusao, $fotografia, $id);
+        return $stmt->execute();
+
+    }
+    //prepara a query SQL, apaga o projeto cujo ID corresponder ao valor enviado
+    public function deleteProjects($id)
+    {
+        $stmt = $this->database->prepare("DELETE FROM projetos WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        return $stmt->execute();
     }
 }
 ?>
